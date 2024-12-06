0858a673423da03495d2f354f239f9e0f1ab4e13
Egor-plsusername <egor.bezuglyi@gmail.com>
2024-12-06 12:55:20 +0000

    commit -m "not done  yet"

--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,7 +1,82 @@
-import java.awt.Graphics;
 import java.awt.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Paddle {
+		private int height;
+		private int width;
+		private int x;
+		private int y;
+	 	private int move;
+		private int getVelocity;
+		private int addVelocity;
+		private int setVelocity;
+				public Paddle(int a, int b, int c) {
+				}
+		
+			public Paddle (int width, int height, int x, int y){
+				this.height = height;
+				this.width = width;
+				this.x = x;
+				this.y = y;
+			}
+		
+			public int getWidth(){
+				return width;
+			}
+			
+			public int getHeight(){
+				return height;
+			}
+			
+			public int getX(){
+				return x;
+			}
+		
+			public int getY(){
+				return y;
+			}
+		
+			public int getVelocity() {
+					return x;
+			}
+		
+			public void setWidth(int width){
+				this.width = width;
+			}
+			
+			public void setHeight(int height){
+				this.height = height;
+			}
+			
+			public void setX(int x){
+				this.x = x;
+			}
+		
+			public void setY(int y){
+				this.y = y;
+			}
+		
+			public void draw(Graphics g){
+				g.setColor(Color.cyan);
+			}
+		
+			public void move() {
+				this.move = getVelocity;
+			}
+		
+			public void addVelocity(int i) {
+				this.addVelocity = getVelocity + getVelocity;
+			}
+		
+			public void setVelocity(int getVelocity) {
+				this.setVelocity = getVelocity;
+	}
+
+
+
+}
 
 	//your code here!
 	
@@ -13,4 +88,4 @@ public class Paddle {
 	//methods:
 	
 	
-}
+
